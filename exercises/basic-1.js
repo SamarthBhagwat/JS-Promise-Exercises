81d6b9287ab5413de692fe312cffb3891d7f3fef
Samarth <samarthrn19@gmail.com>
2024-01-01 17:46:57 +0530

    Added more questions

---
+++ exercises/basic-1.js
@@ -0,0 +1,2 @@
+// The function job must return a promise object (you are in a NodeJS environment, you can use new Promise)
+// The promise must resolve itself 2 seconds after the call to job and must provide hello world in the data
---
+++ exercises/basic-2.js
@@ -0,0 +1,5 @@
+/*Your function must always return a promise
+If data is not a number, return a promise rejected instantly and give the data "error" (in a string)
+If data is an odd number, return a promise resolved 1 second later and give the data "odd" (in a string)
+If data is an even number, return a promise rejected 2 seconds later and give the data "even" (in a string)
+*/
